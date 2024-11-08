e76567cdfb18cf7a5d2bd74d7d13888fde2137da
Thomas Beacock <thomasbeacock@gmail.com>
2024-11-08 18:49:51 +0000

    Setup export config for GitHub pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    basePath: "/visual-coding-web-app",
+    images: {
+        unoptimized: true,
+    }
+};
 
 export default nextConfig;
